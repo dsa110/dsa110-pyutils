5606521aecd36c57fbb7c60422a1653ca3b61609
Casey Law <claw@dsamaster.ovro.pvt>
2020-03-16 06:11:27 -0700

    restructuring for setuptools

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsa110-pyutils',
+      version='0.1',
+      url='http://github.com/dsa110/dsa110-pyutils',
+      packages=['dsautils'],
+      zip_safe=False)
