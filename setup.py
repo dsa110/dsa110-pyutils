4c503d32c787f7c624ef654a7887a6ec7a475cd5
rh <githubrh@pm.me>
2020-03-18 10:36:40 -0700

    Modify setup.py to auto include git tag as version.
    Signed-off-by: rh <githubrh@pm.me>

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
+from version import get_git_version
 
 setup(name='dsa110-pyutils',
-      version='1.0.3',
+      version=get_git_version(),
       url='http://github.com/dsa110/dsa110-pyutils',
       packages=['dsautils'],
       package_data = {
           'dsautils': ['conf/*', 'test/etcdConfig.yml'],
           },
+      tests_require=[
+          'coverage'
+          ],
       entry_points='''
           [console_scripts]
           dsamon=dsautils.cli:mon
