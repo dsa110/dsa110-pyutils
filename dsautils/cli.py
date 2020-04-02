39e93e7948e57b2d57e2d95e336588c55b087a2e
= Casey Law <= caseyjlaw@gmail.com>
2020-04-02 15:49:16 -0700

    working ant moving and noise diode

--- dsautils/cli.py
+++ dsautils/cli.py
@@ -4,6 +4,8 @@ from dsautils import dsa_store
 import dsautils.dsa_syslog as dsl
 
 logger = dsl.DsaSyslogger()    
+logger.subsystem("software")
+logger.app("mnccli")
 de = dsa_store.DsaStore()
 
 # etcd monitor commands
@@ -36,18 +38,21 @@ def snap(snapnum):
     print(vv)
 
 @mon.command()
+@click.argument('subsystem', type=int)
 @click.argument('antnum', type=int)
 @click.option('--timeout', type=int, default=None)
-def watchant(antnum, timeout):
-    """ Wait for antenna state to change
+def watch(subsystem, antnum, timeout):
+    """ Wait for antenna commanded state to change
     """
     def my_cb(event: "Dictionary"):
         print(event)
 
-    vv = de.get_dict('/mon/ant/{0}'.format(antnum))
-    print("Watching antenna {0} for changes from {1}".format(antnum, vv))
+    assert subsystem in ['ant', 'snap']
 
-    de.add_watch('/mon/ant/{0}'.format(antnum), my_cb)
+    vv = de.get_dict('/cmd/{0}/{1}'.format(subsystem, antnum))
+    print("Watching antenna {0} for command. Current commanded values: {1}".format(antnum, vv))
+
+    de.add_watch('/cmd/{0}/{1}'.format(subsystem, antnum), my_cb)
     t0 = time.time()
     if timeout is not None:
         while time.time() - t0 < timeout:
@@ -65,14 +70,46 @@ def con():
 
 
 @con.command()
-@click.argument('subsystem', type=str)
-def listkeys(subsystem):
+@click.argument('antnum', type=str)
+@click.argument('elev', type=float)
+def move(antnum, elev):
+    """ Set an antenna elevation
     """
-    Name of subsystem to list keys
-    """
-    
-    assert subsystem in ['ant']
-    vv = de.get_dict('/mon/{0}/1'.format(subsystem))
 
-#    logger.info(list(vv.keys()))
-    print(list(vv.keys()))
+    logger.info("Commanding ant {0} to move to {1}".format(antnum, elev))
+    de.put_dict('/cmd/ant/{0}'.format(antnum),  {"cmd": "move", "val": elev})
+
+
+@con.command()
+@click.argument('antnum', type=str)
+@click.argument('elev', type=float)
+def halt(antnum):
+    """ Halt motion of antenna
+    """
+
+    logger.info("Commanding ant {0} to move to {1}".format(antnum, elev))
+    de.put_dict('/cmd/ant/{0}'.format(antnum),  {"cmd": "halt", "val": None}) # test this
+
+
+@con.command()
+@click.argument('antnum', type=str)
+@click.argument('ab', type=str)
+@click.argument('value', type=bool)
+def noise(antnum, ab, value):
+    """ Set a the state of noise diode True=on, False=off.
+    """
+
+    assert ab in ['a', 'b']
+    logger.info("Commanding ant {0} noise {1} to {2}".format(antnum, ab, value))
+    de.put_dict('/cmd/ant/{0}'.format(antnum),  {"cmd": "noise_{0}_on".format(ab), "val": value})
+
+
+@con.command()
+@click.argument('snapnum', type=str)
+@click.argument('prop', type=str)
+@click.argument('val', type=str)
+def snap(snapnum, prop, val):
+    """ Set a snap property to new value
+    """
+
+    pass
